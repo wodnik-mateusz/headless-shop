9a98090daa877dace0112ed91105fe37ad07c3d9
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-09-09 20:21:46 +0200

    Add css reset

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.sass';
+import './reset.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
